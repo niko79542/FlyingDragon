51ab3a6edc08dd329b907f0194851176595a1772
niko <jjj.qqqq@gmail.com>
2019-10-05 11:08:45 -0700

    IS San Francisco the capital of the US?

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myToast: Toast
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setListeners()
+    }
+
+    private fun checkAnswer(view: View) {
+        val toastMessage = when (view.id) {
+            R.id.button -> "You are correct!"
+            else -> "Incorrect!"
+        }
+        myToast.cancel()
+        val myToast = Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT)
+        myToast.show()
+
+
+
+    }
+
+    private fun setListeners() {
+        findViewById<Button>(R.id.button).setOnClickListener { view: View ->
+            checkAnswer(view)
+        }
+        findViewById<Button>(R.id.button2).setOnClickListener { view: View ->
+            checkAnswer(view)
+        }
     }
 }
